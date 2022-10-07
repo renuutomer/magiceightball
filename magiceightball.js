9b51f2d710023c8063d710bebaef6887bee837d6
renuu <108678640+renuutomer@users.noreply.github.com>
2022-10-07 13:01:49 +0100

    Create magiceightball.js

---
+++ magiceightball.js
@@ -0,0 +1,37 @@
+var userName = ''
+'Jane' ? console.log('Hello, Jane!') :
+console.log('Hello!');
+var userQuestion = 'Am i good at nitrotype?'
+console.log(`Am i good at nitrotype?: ${userQuestion}`);
+const randomNumber = Math.floor(Math.random() * 8);
+let = ''
+switch (eightBall) {
+  case 0:
+    eightBall = 'It is certain'
+    break;
+  case 1:
+    eightBall = 'It is decidedly so'
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again'
+    break;
+  case 3:
+   eightBall = 'Cannot predict now'
+    break;
+  case 4:
+    eightBall = 'Do not count on it'
+    break;
+  case 5:
+    eightBall = 'My sources say no'
+    break;
+  case 6:
+    eightBall = 'Outlook not so good'
+    break;
+  case 7:
+    eightBall = 'Signs point to yes'
+    break;
+}
+console.log(`The eight ball answered:
+ ${eightBall}`);
+var eightBall = ''
+console.log(eightBall);
